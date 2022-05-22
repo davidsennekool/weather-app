b2caffcbf45a623ea14d33025706d13e7643190a
davidsennekool <davidsennekool@gmail.com>
2022-05-22 22:58:33 +0200

    chore: Removed env

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  corsUrl: 'https://cors-anywhere.herokuapp.com/',
-  baseUrl: 'https://api.openweathermap.org',
-  appId: '04ae8a7fc5c032bd61ecbf61e6681a6a',
-};
