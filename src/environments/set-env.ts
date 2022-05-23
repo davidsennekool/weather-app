0a64ae7b0b7e9c122ab2dc6e33b1587923f77d69
davidsennekool <davidsennekool@gmail.com>
2022-05-23 08:48:21 +0200

    fix: Write to prod config

--- src/environments/set-env.ts
+++ src/environments/set-env.ts
@@ -2,21 +2,21 @@ const setEnv = () => {
   const fs = require('fs');
   const writeFile = fs.writeFile;
 // Configure Angular `environment.ts` file path
-  const targetPath = './src/environments/environment.ts';
+  const targetPath = './src/environments/environment.prod.ts';
 // Load node modules
   const appVersion = require('../../package.json').version;
   require('dotenv').config({
     path: 'src/environments/.env'
   });
 // `environment.ts` file structure
-  const envConfigFile = `export const environment = {
+  const envProdConfigFile = `export const environment = {
     appId: '${process.env['APP_ID']}',
     baseUrl: '${process.env['BASE_URL']}',
     corsUrl: '${process.env['CORS_URL']}',
     production: true,
   };`;
   console.log('The file `environment.ts` will be written with the following content: \n');
-  writeFile(targetPath, envConfigFile, (err: any) => {
+  writeFile(targetPath, envProdConfigFile, (err: any) => {
     if (err) {
       console.error(err);
       throw err;
