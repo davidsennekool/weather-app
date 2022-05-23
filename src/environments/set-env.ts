5ab797b6485a2e4c68cdb88d35b81451611d5936
davidsennekool <davidsennekool@gmail.com>
2022-05-23 08:45:40 +0200

    chore: Removed color code

--- src/environments/set-env.ts
+++ src/environments/set-env.ts
@@ -4,7 +4,6 @@ const setEnv = () => {
 // Configure Angular `environment.ts` file path
   const targetPath = './src/environments/environment.ts';
 // Load node modules
-  const colors = require('colors');
   const appVersion = require('../../package.json').version;
   require('dotenv').config({
     path: 'src/environments/.env'
@@ -16,13 +15,13 @@ const setEnv = () => {
     corsUrl: '${process.env['CORS_URL']}',
     production: true,
   };`;
-  console.log(colors.magenta('The file `environment.ts` will be written with the following content: \n'));
+  console.log('The file `environment.ts` will be written with the following content: \n');
   writeFile(targetPath, envConfigFile, (err: any) => {
     if (err) {
       console.error(err);
       throw err;
     } else {
-      console.log(colors.magenta(`Angular environment.ts file generated correctly at ${targetPath} \n`));
+      console.log(`Angular environment.ts file generated correctly at ${targetPath} \n`);
     }
   });
 };
